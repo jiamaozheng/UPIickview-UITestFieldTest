ef74328c14523a90ecdf6290681ab6fba79dbbfd
Jiamao Zheng <jiamao@emergemedia.com>
2015-07-13 13:18:17 -0500

    fdasfsfa

--- UIPickviewerTest/ViewController.h
+++ UIPickviewerTest/ViewController.h
@@ -4,11 +4,16 @@
 //
 //  Created by Jiamao Zheng on 7/13/15.
 //  Copyright (c) 2015 Emerge Media. All rights reserved.
-//
+// http://stackoverflow.com/questions/26904119/how-to-open-a-pickerviewdropdown-on-uitextview-click-in-ios
+
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIPickerViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *pickViewer;
+
+@property (weak, nonatomic) IBOutlet UILabel *icon;
 
 
 @end
--- UIPickviewerTest/ViewController.m
+++ UIPickviewerTest/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Jiamao Zheng on 7/13/15.
 //  Copyright (c) 2015 Emerge Media. All rights reserved.
-//
+//http://kaflurbaleen.blogspot.com/2014/05/dropdown-menu-like-things-on-ios-made.html
 
 #import "ViewController.h"
 
@@ -16,8 +16,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+     self.icon.text = @"Jiamao";
+    
+    self.pickViewer.inputView = [self createPicker];
+//    self.pickViewer.text = @"MM/DD/YYYY";
+    
+
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+- (UIView *)createPicker {
+    
+    UIView *pickerView = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, 216)];
+    pickerView.backgroundColor = [UIColor grayColor];
+    
+    UIPickerView *picker = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 45, 300, 120)];
+    picker.delegate = self;
+    [pickerView addSubview:picker];
+    
+    return pickerView;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    [self.pickViewer resignFirstResponder];
+    pickerView.hidden = YES; //Show the pickerview again when you neeed it
+}
+
+//- (void) pickerView: (UIPickerView*) pickerView
+//       didSelectRow: (NSInteger)     row
+//        inComponent: (NSInteger)     component
+//{
+//    [self.pickViewer resignFirstResponder];
+//}
+
+//- (BOOL)textFieldShouldReturn:(UITextField *)textField
+//{
+//    if (textField == self.ageTextField)
+//    {
+//        [ageTextField resignFirstResponder];
+//    }
+//    else
+//    {
+//        [relationshipTextField resignFirstResponder];
+//    }
+//    [pickerView removeFromSuperview];
+//    return YES;
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
